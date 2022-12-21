91eb575255a670f07c0698744a26a5319c33d678
Joshua MacDonald <jmacd@users.noreply.github.com>
2022-12-21 10:39:57 -0800

    Runtime metrics support for Go-1.20 (#301)

--- lightstep/instrumentation/runtime/doc.go
+++ lightstep/instrumentation/runtime/doc.go
@@ -12,51 +12,68 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// package runtime geneartes metrics run the Golang runtime/metrics package.
+// package runtime generates metrics from the Golang runtime/metrics package.
 //
-// There are two special policies that are used to translate these
-// metrics into the OpenTelemetry model.
+// There are several conventions used to translate these metrics into
+// the OpenTelemetry model.  Builtin metrics are defined in terms of
+// the expected OpenTelemetry instrument kind in defs.go.
 //
-//  1. The runtime/metrics name is split into its name and unit part;
-//     when there are two metrics with the same name and different
-//     units, the only known case is where "objects" and "bytes" are
-//     present.  In this case, the outputs are a unitless metric (with
-//     suffix, e.g., ending `gc.heap.allocs.objects`) and a unitful
-//     metric with no suffix (e.g., ending `gc.heap.allocs` having
-//     bytes units).
-//  2. When there are >= 2 metrics with the same prefix and one
-//     matching `prefix.total`, the total is skipped and the other
-//     members are assembled into a single Counter or UpDownCounter
-//     metric with multiple attribute values.  The supported cases
-//     are for `class` and `cycle` attributes.
+//  1. Single Counter, UpDownCounter, and Gauge instruments.  No
+//  wildcards are used.  For example:
 //
-// The following metrics are generated in go-1.19.
+//      /cgo/go-to-c-calls:calls
 //
-// Name                                                    Unit          Instrument
-// ------------------------------------------------------------------------------------
-// process.runtime.go.cgo.go-to-c-calls                    {calls}       Counter[int64]
-// process.runtime.go.gc.cycles{cycle=forced,automatic}    {gc-cycles}   Counter[int64]
-// process.runtime.go.gc.heap.allocs                       bytes (*)     Counter[int64]
-// process.runtime.go.gc.heap.allocs.objects               {objects} (*) Counter[int64]
-// process.runtime.go.gc.heap.allocs-by-size               bytes         Histogram[float64] (**)
-// process.runtime.go.gc.heap.frees                        bytes (*)     Counter[int64]
-// process.runtime.go.gc.heap.frees.objects                {objects} (*) Counter[int64]
-// process.runtime.go.gc.heap.frees-by-size                bytes         Histogram[float64] (**)
-// process.runtime.go.gc.heap.goal                         bytes         UpDownCounter[int64]
-// process.runtime.go.gc.heap.objects                      {objects}     UpDownCounter[int64]
-// process.runtime.go.gc.heap.tiny.allocs                  {objects}     Counter[int64]
-// process.runtime.go.gc.limiter.last-enabled              {gc-cycle}    UpDownCounter[int64]
-// process.runtime.go.gc.pauses                            seconds       Histogram[float64] (**)
-// process.runtime.go.gc.stack.starting-size               bytes         UpDownCounter[int64]
-// process.runtime.go.memory.usage{class=...}              bytes         UpDownCounter[int64]
-// process.runtime.go.sched.gomaxprocs                     {threads}     UpDownCounter[int64]
-// process.runtime.go.sched.goroutines                     {goroutines}  UpDownCounter[int64]
-// process.runtime.go.sched.latencies                      seconds       GaugeHistogram[float64] (**)
+//  becomes:
 //
-// (*) Empty unit strings are cases where runtime/metric produces
-// duplicate names ignoring the unit string (see policy #1).
-// (**) Histograms are not currently implemented, see the related
-// issues for an explanation:
+//      process.runtime.go.cgo.go-to-c-calls (unit: {calls})
+//
+//  2. Objects/Bytes Counter.  There are two runtime/metrics with the
+//  same name and different units.  The objects counter has a suffix,
+//  the bytes counter has a unit, to disambiguate.  For example:
+//
+//      /gc/heap/allocs:*
+//
+//  becomes:
+//
+//      process.runtime.go.gc.heap.allocs (unit: bytes)
+//      process.runtime.go.gc.heap.allocs.objects (unitless)
+//
+//  3. Multi-dimensional Counter/UpDownCounter (generally), ignore any
+//  "total" elements to avoid double-counting.  For example:
+//
+//      /gc/cycles/*:gc-cycles
+//
+//  becomes:
+//
+//      process.runtime.go.gc.cycles (unit: gc-cycles)
+//
+//  with two attribute setes:
+//
+//      class=automatic
+//      class=forced
+//
+//  4. Multi-dimensional Counter/UpDownCounter (named ".classes"), map
+//  to ".usage" for bytes and ".time" for cpu-seconds.  For example:
+//
+//      /cpu/classes/*:cpu-seconds
+//
+//  becomes:
+//
+//      process.runtime.go.cpu.time (unit: cpu-seconds)
+//
+//  with multi-dimensional attributes:
+//
+//      class=gc,class2=mark,class3=assist
+//      class=gc,class2=mark,class3=dedicated
+//      class=gc,class2=mark,class3=idle
+//      class=gc,class2=pause
+//      class=scavenge,class2=assist
+//      class=scavenge,class2=background
+//      class=idle
+//      class=user
+//
+// Histograms are not currently implemented, see the related issues
+// for an explanation:
 // https://github.com/open-telemetry/opentelemetry-specification/issues/2713
 // https://github.com/open-telemetry/opentelemetry-specification/issues/2714
 
